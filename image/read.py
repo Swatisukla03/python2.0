336222263ba68c090e01c0835275bbd728d07a7f
Swati <swatisukla16@gmail.com>
2022-01-18 19:14:04 +0530

    first commit

---
+++ image/read.py
@@ -0,0 +1,7 @@
+import cv2 as cv
+img=cv.imread('E:\python\image\wallpaper1.jpg')
+cv.imshow('wallpaper1',img)
+cv.waitkey(2000)
+
+
+
---
+++ python 1/hello.py
@@ -0,0 +1,3 @@
+ import cv2 as cv
+img=cv.imread('python/image/wallpaper1.jpg')
+cv.imshow('wallpaper1',img)
